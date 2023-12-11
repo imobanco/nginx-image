d3a244ff7e0cb305b7c0eb0bb56af9dc7291e7d5
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2023-12-11 14:36:51 -0300

    Refactor: nix flakes + fmt

--- flake.nix
+++ flake.nix
@@ -1,9 +1,20 @@
 {
   description = "This is a nix with flakes package";
+  /*
 
+    nix \
+    flake \
+    update \
+    --override-input nixpkgs github:NixOS/nixpkgs/ea4c80b39be4c09702b0cb3b42eab59e2ba4f24b \
+    --override-input podman-rootless github:ES-Nix/podman-rootless/83ff27be5616e3029c2aec2de595f86da081e857 \
+    --override-input flake-utils github:numtide/flake-utils/5aed5285a952e0b949eb3ba02c12fa4fcfef535f
+  */
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     podman-rootless.url = "github:ES-Nix/podman-rootless/from-nixpkgs";
+
+    podman-rootless.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils, podman-rootless }:
@@ -11,21 +22,33 @@
       let
 
         pkgsAllowUnfree = import nixpkgs {
-          system = "x86_64-linux";
+          inherit system;
           config = { allowUnfree = true; };
         };
       in
       {
 
-        devShell = pkgsAllowUnfree.mkShell {
+        # nix fmt
+        formatter = pkgsAllowUnfree.nixpkgs-fmt;
+
+        devShells.default = pkgsAllowUnfree.mkShell {
           buildInputs = with pkgsAllowUnfree; [
             bashInteractive
+            coreutils
             gnumake
             podman-rootless.packages.${system}.podman
           ];
 
           shellHook = ''
             export TMPDIR=/tmp
+
+            test -d .profiles || mkdir -v .profiles
+
+            test -L .profiles/dev \
+            || nix develop .# --profile .profiles/dev --command true
+
+            test -L .profiles/dev-shell-default \
+            || nix build $(nix eval --impure --raw .#devShells."$system".default.drvPath) --out-link .profiles/dev-shell-"$system"-default
 
             echo "Entering the nix devShell"
           '';
